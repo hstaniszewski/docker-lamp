e21c5129a73186892c53147e87dd4ec5e42f310a
Hubert Staniszewski <hubert@staniszewski.cc>
2019-11-25 19:25:11 +0100

    Sass & watch

--- www/index.php
+++ www/index.php
@@ -1,10 +1,20 @@
-<?php
-    $mysql = new mysqli(
-        "mysql",
-        "root",
-        "example",
-        "db_default"
-    );
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Dev</title>
+    <link rel="stylesheet" href="./style.css"/>
+</head>
+<body>
+    <?php
+        $mysql = new mysqli(
+            "mysql",
+            "root",
+            "example",
+            "db_default"
+        );
 
-    echo "MySQL Server info: ".$mysql->host_info;
-?>
+        echo "MySQL Server info: ".$mysql->host_info;
+    ?>
+</body>
+</html>
